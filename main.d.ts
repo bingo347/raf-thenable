e4a6d60427e4af0ec5cd5516885dff98c4e5b2bb
Dmitriy Belyaev <mail@d-belyaev.ru>
2020-12-23 00:33:58 +0300

    feat: upd interface and add tsdoc

--- main.d.ts
+++ main.d.ts
@@ -1,10 +1,20 @@
-interface CancelablePromise extends Promise<void> {
-    cancel(): void;
+interface CancelableRAFPromise extends Promise<number> {
+    /**
+     * cancelAnimationFrame and optional reject this promise
+     *
+     * @param doNotReject set this to true for cancel without promise rejection
+     */
+    cancel(doNotReject?: boolean): void;
 }
 
-interface ThenableRAF {
-    (callback: FrameRequestCallback): CancelablePromise;
-    then(callback: FrameRequestCallback): CancelablePromise;
+interface ThenableRAF extends PromiseLike<number> {
+    /**
+     * requestAnimationFrame wrapper/polyfill
+     *
+     * @param callback it will be executed directly in the animation frame (before rendering)
+     * @returns Promise with cancel method, it resolved in nearest microtask after animation frame (and after rendering)
+     */
+    (callback?: FrameRequestCallback): CancelableRAFPromise;
 }
 
 declare const thenableRAF: ThenableRAF;
